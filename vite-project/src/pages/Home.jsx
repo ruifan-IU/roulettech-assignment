a0efbf9610ddfcc1a51d90b15c22a4f5551071e6
Rui Fan <ruifan@indiana.edu>
2024-09-12 23:50:57 -0700

    set up react query for fetching chapters

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -3,31 +3,45 @@ import { Outlet } from 'react-router-dom';
 import api from '../api';
 import NavBar from '../components/NavBar';
 import Chapter from '../components/Chapter';
+import LoadingIndicator from '../components/LoadingIndicator';
+import { useQuery } from '@tanstack/react-query';
 
 function Home() {
-  const [chapterList, setChapterList] = useState([]);
+  // const [chapterList, setChapterList] = useState([]);
   const [selectedChapter, setSelectedChapter] = useState(null);
 
-  useEffect(() => {
-    getChapterList();
-  }, []);
+  // useEffect(() => {
+  //   getChapterList();
+  // }, []);
 
-  const getChapterList = () => {
-    api
-      .get('/api/home/')
-      .then((res) => res.data)
-      .then((data) => {
-        setChapterList(data);
-      })
-      .catch((err) => alert(err));
+  const getChapterList = async () => {
+    try {
+      const res = await api.get('/api/home/');
+      const data = res.data;
+      // setChapterList(data);
+      return data;
+    } catch (err) {
+      alert(err);
+    }
   };
 
+  const {
+    isLoading,
+    error,
+    data: chapters,
+  } = useQuery({
+    queryKey: ['chapters'],
+    queryFn: getChapterList,
+  });
+
+  if (isLoading) return <LoadingIndicator />;
+  if (error) return <div>Error: {error.message}</div>;
   return (
     <div>
       <NavBar setSelectedChapter={setSelectedChapter} />
       <div className='display'>
         <div className='display-chapters'>
-          {chapterList.map((chapter) => (
+          {chapters.map((chapter) => (
             <Chapter
               key={chapter.id}
               chapter={chapter}
